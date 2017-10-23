39b328ba0ffe933a0c7d0658f0be2bb09c81dfbd
roginahany <roginahany546@gmail.com>
2017-10-23 21:59:42 +0200

    add copy functions slide 98&99

--- MatrixProject/CMatrix.cpp
+++ MatrixProject/CMatrix.cpp
@@ -10,6 +10,31 @@ CMatrix :: CMatrix ()
 nR = nC = 0;
 values = NULL;
 }
+CMatrix::CMatrix(CMatrix& m)
+{ nR = nC = 0;
+values = NULL;
+copy(m);
+}
+CMatrix::CMatrix(string s)
+{ nR = nC = 0;
+values = NULL;
+copy(s);
+}
+CMatrix::CMatrix(double d)
+{
+	nR = nC = 0;
+	values = NULL;
+	copy(d);
+}
+void CMatrix::copy(double d)
+{
+	reset();
+	this->nR = 1;
+	this->nC = 1;
+	values = new double*[1];
+	values[0] = new double[1];
+	values[0][0] = d;
+}
 
 CMatrix :: ~CMatrix ()
 {
@@ -38,6 +63,22 @@ CMatrix :: CMatrix (int nR, int nC, int initialization, double initializationVal
 			case MI_RAND: values[iR][iC] = (rand()%1000000)/1000000.0;break;
 			case MI_VALUE:values[iR][iC] = initializationValue;break;
 			}
+		}
+	}
+}
+void CMatrix::copy(CMatrix& m)
+{
+	reset();
+	this->nR = m.nR;
+	this->nC = m.nC;
+	if ((nR*nC) == 0){ values = NULL; return; }
+	values = new double*[nR];
+	for (int iR = 0; iR<nR; iR++)
+	{
+		values[iR] = new double[nC];
+		for (int iC = 0; iC<nC; iC++)
+		{
+			values[iR][iC] = m.values[iR][iC];
 		}
 	}
 }
