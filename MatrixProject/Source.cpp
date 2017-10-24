e05bd85e8cf51b4cbb7ba3274161d7f63b98eada
David <davidibrahim8888@gmail.com>
2017-10-25 01:39:32 +0200

    Fix Some Bugs in linking the projects files

--- MatrixProject/Source.cpp
+++ MatrixProject/Source.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
-//ikjhnkj
+#include <iostream>
+#include"CMatrix.h"
+
+using namespace std;
 int main(){ 
-  
 
-
+	CMatrix C = CMatrix (3, 3,CMatrix :: MI_VALUE, 5);
+  cout<<C.getString();
+  return 0;
 }
