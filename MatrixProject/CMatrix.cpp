7690e0ee8080d039dcc2001107a5d0bfe1e3d926
ETBMina <asu.eng.mina@gmail.com>
2017-10-26 20:48:16 +0200

    multiplication and division functions

--- MatrixProject/CMatrix.cpp
+++ MatrixProject/CMatrix.cpp
@@ -151,3 +151,79 @@ CMatrix CMatrix::operator-() {
 		for(int iC=0;iC<nC;iC++) 
 			values[iR][iC] = -values[iR][iC];
 	return *this; }
+
+void CMatrix::mul(CMatrix& m)
+{
+	if (nC != m.nR)
+		throw("Invalid matrix dimension");
+	CMatrix r(nR, m.nC);
+
+	for (int iR = 0; iR<r.nR; iR++)
+		for (int iC = 0; iC<r.nC; iC++)
+		{
+			r.values[iR][iC] = 0;
+				for (int k = 0; k<m.nC; k++)
+					r.values[iR][iC] += values[iR][k] * m.values[k][iC];
+		}
+	copy(r);	
+}
+void CMatrix::operator*=(CMatrix& m)
+{
+	mul(m);
+}
+void CMatrix::operator*=(double d)
+{
+	for (int iR = 0; iR<nR; iR++)
+		for (int iC = 0; iC<nC; iC++)
+			values[iR][iC] *= 9;
+}
+CMatrix CMatrix::operator*(CMatrix& m)
+{
+	CMatrix r = *this;
+	r *= m;
+	return r;
+}
+CMatrix CMatrix::operator*(double d)
+{
+	CMatrix r = *this;
+	r *= d;
+	return r;
+}
+
+void CMatrix::div(CMatrix& m)
+{
+	if (nC != m.nR)
+		throw("Invalid matrix dimension");
+	CMatrix r(nR, m.nC);
+
+	for (int iR = 0; iR<r.nR; iR++)
+		for (int iC = 0; iC<r.nC; iC++)
+		{
+			r.values[iR][iC] = 0;
+			for (int k = 0; k<m.nC; k++)
+				r.values[iR][iC] += values[iR][k] / m.values[k][iC];
+		}
+	copy(r);
+}
+void CMatrix::operator/=(CMatrix& m)
+{
+	div(m);
+}
+void CMatrix::operator/=(double d)
+{
+	for (int iR = 0; iR<nR; iR++)
+		for (int iC = 0; iC<nC; iC++)
+			values[iR][iC] /= 9;
+}
+CMatrix CMatrix::operator/(CMatrix& m)
+{
+	CMatrix r = *this;
+	r /= m;
+	return r;
+}
+CMatrix CMatrix::operator/(double d)
+{
+	CMatrix r = *this;
+	r /= d;
+	return r;
+}
