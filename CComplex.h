fdf2c014ccb5a23dd101c7326517b1ae273bc8ad
David <davidibrahim8888@gmail.com>
2017-11-24 20:04:36 +0200

    new branch to support Complex numbers

---
+++ CComplex.h
@@ -0,0 +1,16 @@
+#ifndef CCOMPLEX_H
+#define CCOMPLEX_H
+
+
+class CComplex
+{
+    public:
+        CComplex();
+        virtual ~CComplex();
+
+    protected:
+
+    private:
+};
+
+#endif // CCOMPLEX_H
