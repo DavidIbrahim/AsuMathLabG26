a0b3177e67993ddef4d221931aa0b01d088590d2
martinaihab <martina.ihab96@gmail.com>
2017-11-26 17:14:21 +0200

    Basic Complex Class Description

--- CComplex.cpp
+++ CComplex.cpp
@@ -1,11 +1,89 @@
 #include "CComplex.h"
+#include <string>
+#include <math.h>
+#include <stdio.h>
 
 CComplex::CComplex()
 {
-    //ctor
+    R = I = 0.0;
 }
 
 CComplex::~CComplex()
 {
     //dtor
+}
+
+CComplex::CComplex(double R, double I)
+{
+    this->R = R; this->I = I;
+}
+
+CComplex::CComplex(CComplex& C)
+{
+    copy(C);
+}
+
+void CComplex::copy(CComplex& C)
+{
+    R = C.R; I = C.I;
+}
+
+string CComplex::getString()
+{
+    char text[100];
+    if(I==0)sprintf(text, "%g", R);
+    else if(R==0)sprintf(text, "%g * i", I);
+    else if(I>0)sprintf(text, "%g + %g * i", R, I);
+    else if(I<0)sprintf(text, "%g - %g * i", R, -I);
+    return string(text);
+}
+
+double CComplex::magnitude()
+{
+    return sqrt(R*R+I*I);
+}
+double CComplex::angle()
+{
+    return atan2(I, R);
+}
+
+void CComplex::negative()
+{
+    R*=-1; I*=-1;
+}
+
+double CComplex::real()
+{
+    return R;
+}
+
+double CComplex::imaginary()
+{
+    return I;
+}
+
+CComplex CComplex::addComplex(CComplex& A, CComplex& B)
+{
+    CComplex C;
+    C.R = A.R + B.R;
+    C.I = A.I + B.I;
+    return C;
+}
+
+void CComplex::add(CComplex& C)
+{
+    R += C.R;I += C.I;
+}
+
+CComplex CComplex::subComplex(CComplex& A, CComplex& B)
+{
+    CComplex C;
+    C.R = A.R - B.R;
+    C.I = A.I - B.I;
+    return C;
+}
+
+void CComplex::sub(CComplex& C)
+{
+    R -= C.R;I -= C.I;
 }
--- CComplex.h
+++ CComplex.h
@@ -1,16 +1,35 @@
 #ifndef CCOMPLEX_H
 #define CCOMPLEX_H
+#include <string>
 
+using namespace std;
 
 class CComplex
 {
     public:
         CComplex();
         virtual ~CComplex();
+        CComplex(double R, double I);
+        CComplex(CComplex& C);
+        void copy(CComplex& C);
+        string getString();
+        double magnitude();
+        double angle();
+        void negative();
+        double real();
+        double imaginary();
+        static CComplex addComplex(CComplex& A, CComplex& B);
+        void add(CComplex& C);
+
+        static CComplex subComplex(CComplex& A, CComplex& B);
+        void sub(CComplex& C);
+
 
     protected:
 
     private:
+        double R;
+        double I;
 };
 
 #endif // CCOMPLEX_H
