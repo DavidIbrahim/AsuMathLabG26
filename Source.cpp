9141f62ebb647f94733fadd40cd1b3cc8f87622a
mmmiiinnnaaa <dvsvsvfssgds@gmail.com>
2018-01-27 16:52:08 +0200

    adding the functions of findTheClosingBracketFromAspecificPostion   and trimAllSpacesExceptMatrix also for the function of expretions testing and changing some factors and for power matrix function

--- Source.cpp
+++ Source.cpp
@@ -7,13 +7,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include "Matlab.h"
+#include "conio.h"
 
 
 
 using namespace std;
 int main(int argc, char*argv[])
 {
-      Matlab x;
-        cout<<x.getStringMatrix("[[2 5] [2 5] [2 5]]");
+    CMatrix q("[1 2;1 2]") ;
+
+    cout<<q.getString();
+    CMatrix v ;
+
+    v = q.power(3);
+    cout<<v.getString();
+
+    Matlab x;
+    string s="s s s [ [here no problem    at all ]]          hq               [ [here 2] ]  herenoway  [why ]  [ not  ] +[here]";
+     cout<<s<<endl;
+    x.trimAllSpacesExceptMatrix(s);
+     cout<<s<<endl;
+     getch();
+    cout<<x.solvetrignometry("8-sqrt(2+2)+cos(5)+8")<<endl;
+
+
+   //  x.findTheClosingBracketFromAspecificPostion( "85", 5,'[');
+
+
+
+
+
+/*
+CComplex a(1,0);
+  cout<<a.getString(1);
+CComplex l = a.power(-.2);
+  cout<<l.getString(0);
+*/
+
+
+
+
+
+
+
+
+
+
+    getch();
+    //Matlab x;
+    //cout<<x.getStringMatrix("[[2 5] [2 5] [2 5]]");
+
+
+
+
     return 0;
 }
